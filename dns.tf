d07dc180ee8796b1431a020078ea98e859781876
Jason Haugland <jasonhaugland@gmail.com>
2022-09-09 14:55:30 -0500

    cleanup

--- dns.tf
+++ dns.tf
@@ -11,14 +11,14 @@ locals {
 resource "azurerm_dns_zone" "base" {
   count               = local.full_zone
   name                = local.cluster-base-domain
-  resource_group_name = "${azurerm_resource_group.resource.name}"
+  resource_group_name = local.cluster-base-resource-group
 }
 
 resource "azurerm_dns_a_record" "fixedip" {
   count = var.node-count
   name = "ns${count.index}.${local.cluster-name}"
   zone_name           = local.cluster-base-domain
-  resource_group_name  = "${(local.cluster-base-resource-group != null ? local.cluster-base-resource-group : azurerm_resource_group.resource.name)}"
+  resource_group_name = local.cluster-base-resource-group
   records =  [ "${element(data.azurerm_public_ip.fixedip.*.ip_address, count.index)}" ]
   # records = [ "${element(azurerm_network_interface.nic.*.private_ip_address, count.index)}" ]
   ttl                 = 300
@@ -29,7 +29,7 @@ resource "azurerm_dns_ns_record" "fixedip" {
   count = var.node-count
   name                = "${local.cluster-name}"
   zone_name           = "${local.cluster-base-domain}"
-  resource_group_name  = "${(local.cluster-base-resource-group != null ? local.cluster-base-resource-group : azurerm_resource_group.resource.name)}"
+  resource_group_name = local.cluster-base-resource-group
   ttl                 = 300
   # Has to have trailing periods on each record
   records = formatlist("%s.${local.cluster-base-domain}.", azurerm_dns_a_record.fixedip.*.name)
